6d6b2ec12edac9ee360e9f760bc310bd0e2555ad
AJAY_SINGH_RATHORE <96069023+ajay-singh-rathore@users.noreply.github.com>
2023-04-29 14:52:25 +0530

    Add files via upload

---
+++ Laptop-Price-Prediction/Home.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+def space():
+    st.markdown("<br>", unsafe_allow_html=True)
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="😎",
+)
+
+
+st.write("# Hey Everyone ! 👋")
+
+st.markdown("# :red[AJAY SINGH]")
+st.subheader(" :blue[Data Analyst]")
+space()
+
+
+st.markdown(
+    """
+    #### I specialize in Data Science and Machine Learning and my passion is to take a cup of coffee and start finding some meaningful insight in messy data and build some great model...
+    """
+)
+
+space()
+
+st.write("#### Connect me on [LinkedIn](https://www.linkedin.com/in/ajay-singh-b13175200/)")
+st.write("#### Follow me on [GitHub](https://github.com/ajay-singh-rathore/)")
